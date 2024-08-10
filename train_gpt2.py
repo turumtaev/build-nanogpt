30a2599e0864324c15a296b1b6001a5da5ad3cbf
turumtaev <turumtaev.gz@gmail.com>
2024-08-10 14:25:38 +0300

    more aggresive lr scheduler

--- train_gpt2.py
+++ train_gpt2.py
@@ -440,15 +440,16 @@ min_lr = max_lr * 0.1
 warmup_steps = 715
 # from the same amount of text new tokenizer creates ~5 times more training data
 max_steps = 100000 # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
+lr_max_steps = 25000
 def get_lr(it):
     # 1) linear warmup for warmup_iters steps
     if it < warmup_steps:
         return max_lr * (it+1) / warmup_steps
     # 2) if it > lr_decay_iters, return min learning rate
-    if it > max_steps:
+    if it > lr_max_steps:
         return min_lr
     # 3) in between, use cosine decay down to min learning rate
-    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    decay_ratio = (it - warmup_steps) / (lr_max_steps - warmup_steps)
     assert 0 <= decay_ratio <= 1
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
     return min_lr + coeff * (max_lr - min_lr)
